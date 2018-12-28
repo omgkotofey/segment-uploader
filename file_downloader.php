d0d682cb4c60330b97eab953a836c1dfcb45c1d8
Simple User <u490524@pro40.hs.shared.masterhost.ru>
2018-12-28 22:56:20 +0300

    Теперь файл загружается с уникальным именем

--- file_downloader.php
+++ file_downloader.php
@@ -4,16 +4,18 @@ require_once 'vendor/autoload.php';
 
 use Classes\Config;
 
-ini_set('error_reporting', E_ALL);
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-
+// файл обязательно деолжен придти и быть текстовым
 if (!empty($_FILES) && ($_FILES['mac-file']['type'] == 'text/plain')){
 
-	$uploadfile = Config::get('UPLOAD_DIR') . basename($_FILES['mac-file']['name']);
+	// генерируем уникальное имя для загружаемого файла 
+	$unique_upload_filename = implode(".", [ time().uniqid(rand()) , pathinfo(basename($_FILES["mac-file"]["name"]), PATHINFO_EXTENSION)]);
 
-	if (move_uploaded_file($_FILES['mac-file']['tmp_name'], $uploadfile)) {
-		echo json_encode(['result' => 'success', 'message' => 'Файл загружен успешно', 'filename' => $_FILES['mac-file']['name']]);
+	// загружаем файл в соответствубщую папку
+	$upload_file_path = Config::get('UPLOAD_DIR') . $unique_upload_filename;
+
+	// возвращаем JSON
+	if (move_uploaded_file($_FILES['mac-file']['tmp_name'], $upload_file_path)) {
+		echo json_encode(['result' => 'success', 'message' => 'Файл загружен успешно', 'filename' => $unique_upload_filename]);
 	} 
 	else {
 		echo json_encode(['result' => 'error', 'message' => 'Невозможно загрузить данный файл. Возможно он поврежден или некорректен']);
